cb2de0d74173be284bb4b6da71706a20b076e78d
Benton <bentonrochester@gmail.com>
2012-04-14 14:30:55 -0600

    add .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simplesubscribe::Application.config.secret_token = '26931b22ad36c00a210917a5d79a3bdfcf69a1dee3b17c1bb17df6c576f86dfdf068fe12c088e3b7ec03da6c2e89550c3686d629f13953e6cb4d57c854d66b89'
